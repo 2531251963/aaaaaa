344336561679c1c2e827dad7874dff7b0e388ca2
2531251963 <2531251963@qq.com>
2020-01-06 23:23:35 +0800

    Initial commit

---
+++ src/aa/A.java
@@ -0,0 +1,14 @@
+package aa;
+
+/**
+ * @ClassName A
+ * @Description TODO
+ * @Author Liyihe
+ * @Date 2020/01/06 下午11:22
+ * @Version 1.0
+ */
+public class A {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
